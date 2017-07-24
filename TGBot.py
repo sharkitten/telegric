1d196731cc06d73d34a9b7eaf45f66179049ad34
Christian Heller <c.heller@plomlompom.de>
2017-07-24 18:36:07 +0200

    Make whitespace style consistent and follow PEP8.

--- TGBot.py
+++ TGBot.py
@@ -1,47 +1,50 @@
 import telepot
 
+
 class TGBot(telepot.Bot):
-	def __init__(self, config):
-		self.singleMode = config['general']['single_mode'] == 'True'
-		config = config['telegram']
-		self.sender = config['sender']
-		self.chatID = int(config['chatid'])
-		self.bot = telepot.Bot(config['token'])
-		self.users = set()
+    def __init__(self, config):
+        self.singleMode = config['general']['single_mode'] == 'True'
+        config = config['telegram']
+        self.sender = config['sender']
+        self.chatID = int(config['chatid'])
+        self.bot = telepot.Bot(config['token'])
+        self.users = set()
 
-	def setIRCbot(self, bot):
-		self.ircbot = bot
+    def setIRCbot(self, bot):
+        self.ircbot = bot
 
-	def start(self):
-                from telepot.loop import MessageLoop
-                MessageLoop(self.bot, self.handle).run_forever()
+    def start(self):
+        from telepot.loop import MessageLoop
+        MessageLoop(self.bot, self.handle).run_forever()
 
-	def handle(self, msg):
-		if (not self.ircbot.conn.is_connected()):
-			self.ircbot.conn.connected_checker()
-		print(msg)
-		self.users.add(msg['chat']['username'])
-		if (msg['chat']['id']==self.chatID):
-			self.ircbot.sendMessage(self.ircbot.channel, self.formatMessage(msg))
-		else:
-			print('message received from non-handled chat with ID {}'.format(msg['chat']['id']))
+    def handle(self, msg):
+        if (not self.ircbot.conn.is_connected()):
+            self.ircbot.conn.connected_checker()
+        print(msg)
+        self.users.add(msg['chat']['username'])
+        if msg['chat']['id'] == self.chatID:
+            self.ircbot.sendMessage(self.ircbot.channel,
+                                    self.formatMessage(msg))
+        else:
+            print('message received from non-handled chat with ID {}'.
+                  format(msg['chat']['id']))
 
-	def formatMessage(self, msg):
-		try:
-			body = msg['text']
-		except KeyError:
-			try:
-				body = msg['sticker']['emoji']
-			except KeyError:
-				body = 'message type not supported'
-		if self.singleMode:
-			return body
+    def formatMessage(self, msg):
+        try:
+            body = msg['text']
+        except KeyError:
+            try:
+                body = msg['sticker']['emoji']
+            except KeyError:
+                body = 'message type not supported'
+        if self.singleMode:
+            return body
 
-		try:
-			sender = msg['from'][self.sender]
-		except:
-			sender = msg['from']['first_name']
-		return '<{}> {}'.format(sender, body)
+        try:
+            sender = msg['from'][self.sender]
+        except:
+            sender = msg['from']['first_name']
+        return '<{}> {}'.format(sender, body)
 
-	def send(self, msg):
-		self.bot.sendMessage(self.chatID, msg)
+    def send(self, msg):
+        self.bot.sendMessage(self.chatID, msg)
