18300ab24abfbf494dd7ecc3a54410166d0573d0
Christian Heller <c.heller@plomlompom.de>
2017-08-07 20:00:20 +0200

    lots of stuff (#1)
    * Add requirements for pip install.
    
    * Fix failures that come from obsolete API assumptions.
    
    * Use non-deprecated telepot interface for message loop.
    
    * Make whitespace style consistent and follow PEP8.
    
    * Add some documentation via docstrings, comments.
    
    * Refactor IRCBot class to more conventional subclassing style.
    
    * Re-implement msg.from.username nick adding, depending on chat type.
    
    * Drop automatic @-prefixing of usernames.
    
    * Remove non-functional connected_checker code.
    
    * Remove telegram.sender config option, hardcode 'username' field name.
    
    * Refactor TGBot class to more conventional subclassing style.
    
    * Remove unused module import.

--- IRCbot.py
+++ IRCbot.py
@@ -1,82 +1,96 @@
 import irc.bot
 import sys
-import irc.strings
+
 
 class IRCBot(irc.bot.SingleServerIRCBot):
-	def __init__(self, config):
-		self.singleMode = config['general']['single_mode'] == 'True'
-		config = config['irc']
-		self.server = config['server']
-		self.port = int(config['port'])
-		self.nickname = config['nickname']
-		self.client = irc.bot.SingleServerIRCBot([(self.server, self.port)], self.nickname, self.nickname)
-		self.channel = config['channel']
-		self.conn = self.client.connection
+    def __init__(self, config):
+        """Set attributes from config file, start connection."""
+        self.singleMode = config['general']['single_mode'] == 'True'
+        config = config['irc']
+        self.server = config['server']
+        self.port = int(config['port'])
+        self.nickname = config['nickname']
+        self.channel = config['channel']
+        irc.bot.SingleServerIRCBot.__init__(self, [(self.server, self.port)],
+                                            self.nickname, self.nickname)
+        self.register_handlers()
 
-	def register_handlers(self):
-		self.conn.add_global_handler('welcome', self.on_welcome)
-		self.conn.add_global_handler('nicknameinuse', self.on_nicknameinuse)
-		self.conn.add_global_handler('privmsg', self.on_privmsg)
-		self.conn.add_global_handler('pubmsg', self.on_message)
-		self.conn.add_global_handler('action', self.on_message)
-		self.conn.add_global_handler('part', self.on_message)
-		self.conn.add_global_handler('quit', self.on_message)
-		self.conn.add_global_handler('join', self.on_message)
-		self.conn.add_global_handler('topic', self.on_message)
-		self.conn.add_global_handler('disconnect', self.on_disconnect)
+    def register_handlers(self):
+        """Add handler methods for various IRC protocol events.
 
-	def start(self):
-		self.register_handlers()
-		self.client.start()
+        Note that handlers of name 'on_' + event name are added automatically,
+        so lines for 'nicknameinuse', 'privmsg', 'disconnect' would just add
+        double handlers for on_nicknameinuse, on_privmsg, on_disconnect.
+        """
+        self.connection.add_global_handler('welcome', self.on_welcome)
+        self.connection.add_global_handler('pubmsg', self.on_message)
+        self.connection.add_global_handler('action', self.on_message)
+        self.connection.add_global_handler('part', self.on_message)
+        self.connection.add_global_handler('quit', self.on_message)
+        self.connection.add_global_handler('join', self.on_message)
+        self.connection.add_global_handler('topic', self.on_message)
 
-	def on_nicknameinuse(self, c, e):
-		c.nick(c.get_nickname() + "_")
+    def on_nicknameinuse(self, c, e):
+        """When nick is in use, reset it via NICK with '_' suffix added."""
+        # TODO: What when nickname + _ grows beyond maximum allowed size?
+        c.nick(c.get_nickname() + "_")
 
-	def on_welcome(self, c, e):
-		c.join(self.channel)
+    def on_welcome(self, c, e):
+        """On IRC welcome event, join self.channel."""
+        c.join(self.channel)
 
-	def on_privmsg(self, conn, event):
-		if self.singleMode:
-			self.TGbot.send(self.formatMessage(event))
-		else:
-			self.sendMessage(self.getNickname(event), "I'm a bot. I have nothing to say.")
+    def on_privmsg(self, conn, event):
+        """When self.singleMode, format event to TGbot message, else deny."""
+        if self.singleMode:
+            self.TGbot.send(self.formatMessage(event))
+        else:
+            self.sendMessage(self.getNickname(event),
+                             "I'm a bot. I have nothing to say.")
 
-	def on_message(self, conn, event):
-		self.TGbot.send(self.formatMessage(event))
+    def on_message(self, conn, event):
+        """Format event to TGbot message."""
+        self.TGbot.send(self.formatMessage(event))
 
-	def formatMessage(self, msg):
-		sender = self.getNickname(msg)
-		body = ''.join(msg.arguments)
-		type = msg.type
+    def formatMessage(self, msg):
+        """Parse IRC message to TG message string.
 
-		if type == 'action':
-			body = '/me ' + body
-		elif type == 'privmsg':
-			body = 'QUERY: ' + body
-		elif type == 'join':
-			return '{} joined'.format(self.getNickname(msg))
-		elif type == 'part' or type == 'quit':
-			return '{} left'.format(self.getNickname(msg))
+        JOIN/PART messages become self.getNickname(msg) + 'joined'/'parted'
+        strings.
 
-		for user in self.TGbot.users:
-			body = str.replace(body, user, '@'+user)
+        ACTION messages get a '/me' prefix, PRIVMSG messages a 'QUERY: ' one.
+        String subpatterns that are in self.TGbot.users get a '@' prefix. All
+        non-JOIN/PART messages thus created get a further <>-delimited
+        self.getNickname(msg) prefix.
+        """
+        sender = self.getNickname(msg)
+        body = ''.join(msg.arguments)
+        type = msg.type
 
-		return '<{}> {}'.format(sender, body)
+        if type == 'action':
+            body = '/me ' + body
+        elif type == 'privmsg':
+            body = 'QUERY: ' + body
+        elif type == 'join':
+            return '{} joined'.format(self.getNickname(msg))
+        elif type == 'part' or type == 'quit':
+            return '{} left'.format(self.getNickname(msg))
 
-	def setTGbot(self, bot):
-	    self.TGbot = bot
+        return '<{}> {}'.format(sender, body)
 
-	def getNickname(self, event):
-		return event.source.split('!')[0]
+    def setTGbot(self, bot):
+        """Set self.TGbot to bot, assuming it's an TGbot instance."""
+        self.TGbot = bot
 
-	def on_disconnect(self, c, e):
-		self.connect()
+    def getNickname(self, event):
+        return event.source.split('!')[0]
 
-	def connect(self):
-		self.client.connect()
+    def on_disconnect(self, c, e):
+        """Reconnect client to IRC network."""
+        self.connect()
 
-	def sendMessage(self, recipient, msg):
-		try:
-			self.conn.privmsg(recipient, msg)
-		except Exception as e:
-			sys.stderr.write(str(e) + '\n')
+    def sendMessage(self, recipient, msg):
+        """Try sending msg to recipient via PRIVMSG, print error on failure."""
+        try:
+            self.connection.privmsg(recipient, msg)
+        except Exception as e:
+            sys.stderr.write(str(e) + '\n')
--- TGBot.py
+++ TGBot.py
@@ -1,46 +1,61 @@
 import telepot
 
+
 class TGBot(telepot.Bot):
-	def __init__(self, config):
-		self.singleMode = config['general']['single_mode'] == 'True'
-		config = config['telegram']
-		self.sender = config['sender']
-		self.chatID = int(config['chatid'])
-		self.bot = telepot.Bot(config['token'])
-		self.users = set()
+    def __init__(self, config):
+        """Set TG bot attributes from config file."""
+        self.singleMode = config['general']['single_mode'] == 'True'
+        config = config['telegram']
+        self.chatID = int(config['chatid'])
+        telepot.Bot.__init__(self, config['token'])
 
-	def setIRCbot(self, bot):
-		self.ircbot = bot
+    def setIRCbot(self, bot):
+        """Set self.ircbot to bot, assuming it's an IRCbot instance."""
+        self.ircbot = bot
 
-	def start(self):
-		self.bot.message_loop(self.handle)
+    def start(self):
+        """Start loop handling incoming TG messages with self.handle."""
+        from telepot.loop import MessageLoop
+        MessageLoop(self, self.handle).run_forever()
 
-	def handle(self, msg):
-		if (not self.ircbot.conn.is_connected()):
-			self.ircbot.conn.connected_checker()
-		print(msg)
-		self.users.add(msg['from']['username'])
-		if (msg['chat']['id']==self.chatID):
-			self.ircbot.sendMessage(self.ircbot.channel, self.formatMessage(msg))
-		else:
-			print('message received from non-handled chat {} with ID {}'.format(msg['chat']['title'], msg['chat']['id']))
+    def handle(self, msg):
+        """Process TG message to possible IRCbot output.
 
-	def formatMessage(self, msg):
-		try:
-			body = msg['text']
-		except KeyError:
-			try:
-				body = msg['sticker']['emoji']
-			except KeyError:
-				body = 'message type not supported'
-		if self.singleMode:
-			return body
+        Sends msg formatted with self.formatMessage to self.ircbot if
+        msg.chat.id == self.chatID, otherwise prints a failure msg.
+        """
+        print(msg)
+        if msg['chat']['id'] == self.chatID:
+            self.ircbot.sendMessage(self.ircbot.channel,
+                                    self.formatMessage(msg))
+        else:
+            print('message received from non-handled chat with ID {}'.
+                  format(msg['chat']['id']))
 
-		try:
-			sender = msg['from'][self.sender]
-		except:
-			sender = msg['from']['first_name']
-		return '<{}> {}'.format(sender, body)
+    def formatMessage(self, msg):
+        """Parse TG message msg to IRC message string.
 
-	def send(self, msg):
-		self.bot.sendMessage(self.chatID, msg)
+        Will parse msg.text or msg.stricker.emoji fine, otherwise set a
+        'message type not supported' message. On self.singleMode, will return
+        only this message body, otherwise will prepend a <>-delimited name;
+        either msg.from.username (if present), or msg.from.first_name.
+        """
+        try:
+            body = msg['text']
+        except KeyError:
+            try:
+                body = msg['sticker']['emoji']
+            except KeyError:
+                body = 'message type not supported'
+        if self.singleMode:
+            return body
+
+        try:
+            sender = msg['from']['username']
+        except:
+            sender = msg['from']['first_name']
+        return '<{}> {}'.format(sender, body)
+
+    def send(self, msg):
+        """Send message msg as TG message to chat identified by self.chatID."""
+        self.sendMessage(self.chatID, msg)
